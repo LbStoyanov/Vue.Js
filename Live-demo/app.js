7548308d119f20e6056492a3997e9bdbb0570ea0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-05 21:46:07 +0200

    VueJS-first-steps

--- Live-demo/app.js
+++ Live-demo/app.js
@@ -17,11 +17,20 @@ buttonEl.addEventListener('click', addGoal); */
 
 //The same task app with VueJS:
 
+
+
+
 Vue.createApp({
     data(){
         return{
             goals: [],
             enteredValue: ''
         };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
     }
-});
+}).mount('#app');
+
