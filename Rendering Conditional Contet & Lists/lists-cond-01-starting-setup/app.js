d49f667672a1813509de57f52e203cfc3b88721a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-12 21:38:31 +0200

    Loop through

---
+++ Rendering Conditional Contet & Lists/lists-cond-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      goals: [],
+      enteredGoalValue: ''
+    };
+  },
+  methods: {
+    addGoal(){
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = '';
+    },
+  },
+ 
+});
+
+app.mount('#user-goals');
