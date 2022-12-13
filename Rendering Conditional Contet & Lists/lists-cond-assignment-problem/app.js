cb8c76a1edf6300d04a6ed61e9ab31d13a3079b0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-13 17:18:53 +0200

    VueJSPractice

--- Rendering Conditional Contet & Lists/lists-cond-assignment-problem/app.js
+++ Rendering Conditional Contet & Lists/lists-cond-assignment-problem/app.js
@@ -2,13 +2,22 @@ const app = Vue.createApp({
     data(){
         return{
             tasks: [],
-            userInput: ''
+            userInput: '',
+            taskListIsVisible: true
         };
+    },
+    computed: {
+        buttonCaption() {
+            return this.taskListIsVisible ? 'Hide List' : 'Show List';
+        }
     },
     methods:{
         addTask(){
             this.tasks.push(this.userInput);
             this.userInput = '';
+        },
+        toggleList(){
+            this.taskListIsVisible = !this.taskListIsVisible;
         }
     },
 });
