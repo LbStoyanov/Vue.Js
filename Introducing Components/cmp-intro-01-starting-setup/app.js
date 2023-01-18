22ddc8745bb05954a82b99831f9cb3be3fa53e67
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-18 19:12:03 +0200

    Vue Components

--- Introducing Components/cmp-intro-01-starting-setup/app.js
+++ Introducing Components/cmp-intro-01-starting-setup/app.js
@@ -1,36 +1,51 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            detailsAreVisible: false,
-
-            friends: [{
-                id: 'Peshko',
-                name: 'Peshko Dobriq',
-                phone: '5645645645456',
-                email: 'Peshko@Dobriq.com',
-            },
-            {
-                id: 'Goshko',
-                name: 'Goshko Loshiq',
-                phone: '5645645645456',
-                email: 'Peshko@Dobriq.com',
-            },
-            {
-                id: 'Mishko',
-                name: 'Mishko Sredniq',
-                phone: '5645645645456',
-                email: 'Peshko@Dobriq.com',
-            },
-            
-            
-        ],
-        }
-    },
-    methods:{
-        toggleDetails(){
-            this.detailsAreVisible = !this.detailsAreVisible;
-        }
-    },
+  data() {
+    return {
+      friends: [
+        
+        {
+          id: "Goshko",
+          name: "Goshko Loshiq",
+          phone: "5645645645456",
+          email: "Peshko@Dobriq.com",
+        },
+        {
+          id: "Mishko",
+          name: "Mishko Sredniq",
+          phone: "5645645645456",
+          email: "Peshko@Dobriq.com",
+        },
+      ],
+    };
+  },
+  methods: {},
 });
 
-app.mount('#app')
+app.component("friend-contact", {
+  template: `
+    <li>
+    <h2>{{ friend.name}}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Peshko",
+        name: "Peshko Dobriq",
+        phone: "5645645645456",
+        email: "Peshko@Dobriq.com",
+      },
+    };
+  },
+  toggleDetails() {
+    this.detailsAreVisible = !this.detailsAreVisible;
+  },
+});
+
+app.mount("#app");
