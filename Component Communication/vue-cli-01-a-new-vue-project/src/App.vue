f92843c85bac5629bbf720a402c72fedc4e0da99
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-21 18:08:57 +0200

    Vue CLI

--- Component Communication/vue-cli-01-a-new-vue-project/src/App.vue
+++ Component Communication/vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,3 +1,32 @@
+<!-- eslint-disable vue/valid-template-root -->
 
+<template>
+    <h2>My Friends</h2>
+    <ul>
+        <li></li>
+    </ul>
+</template>
 
-<script></script>
+<script>
+
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenzo",
+          phone: "1545454545454554",
+          email: "manu@abv.com",
+        },
+        {
+          id: "Konchev",
+          name: "Manuel Konchev",
+          phone: "1545454545454554",
+          email: "konchev@abv.com",
+        },
+      ],
+    };
+  },
+};
+</script>
