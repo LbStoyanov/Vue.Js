c758276d3ca5c1d2f2326d9060e23b7add183dd6
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-29 22:26:32 +0200

    Learning Resources App

--- The Learning Resources App/prj-cmp-01-starting-setup/src/App.vue
+++ The Learning Resources App/prj-cmp-01-starting-setup/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <ul>
-    <learning-resource v-for="res in storedResources" :key="res.id"></learning-resource>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
   </ul>
 </template>
 
@@ -9,7 +15,7 @@ import LearningResource from './components/learning-resources/LearningResource.v
 
 export default {
   components: {
-    LearningResource
+    LearningResource,
   },
   data() {
     return {
@@ -31,3 +37,19 @@ export default {
   },
 };
 </script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
