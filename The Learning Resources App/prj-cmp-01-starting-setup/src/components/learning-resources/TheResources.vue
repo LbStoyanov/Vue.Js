0aae3e55addbfcde7f62dd570fd90457fa83be6b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-02-01 21:29:34 +0200

    Components

--- The Learning Resources App/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ The Learning Resources App/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,30 +1,52 @@
 <template>
-    <base-card>
-        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
-    </base-card>
-    <component :is="selectedTab"></component>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
 </template>
-
 
 <script>
 import StoredResources from './StoredResources.vue';
 import AddResource from './AddResource.vue';
 
 export default {
-    components:{
-        StoredResources,
-        AddResource
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guid',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide (){
+    return {
+        resources: this.storedResources,
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
     },
-    data () {
-        return {
-            selectedTab: 'stored-resources'
-        };
-    },
-    methods: {
-        setSelectedTab (tab){
-            this.selectedTab = tab;
-        }
-    }
-}
+  },
+};
 </script>
