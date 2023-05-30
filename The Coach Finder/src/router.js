cbd83175763a503d43c5dd3f762d37d299cdca66
LbStoyanov <lbstoyanov1@gmail.com>
2023-05-30 21:13:01 +0300

    Load components

--- The Coach Finder/src/router.js
+++ The Coach Finder/src/router.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import {CoachDetail} from './pages/coaches/CoachDetail.vue';
-import {CoachesList} from './pages/coaches/CoachesList.vue';
-import {CoachRegistration} from './pages/coaches/CoachRegistration.vue';
-import {ContactCoach} from './pages/requests/ContactCoach.vue';
-import {RequestsReceived} from './pages/requests/RequestsReceived.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 
 const router = createRouter({
   history: createWebHistory(),
