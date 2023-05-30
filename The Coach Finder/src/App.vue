6a4d9c0d184c5c4be83c8e110e1b9d54c594bdea
LbStoyanov <lbstoyanov1@gmail.com>
2023-05-30 21:47:26 +0300

    Adding components

--- The Coach Finder/src/App.vue
+++ The Coach Finder/src/App.vue
@@ -1,6 +1,38 @@
 <template>
-    <router-view></router-view>
-  </template>
-  
-  
-  
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- The Coach Finder/src/router.js
+++ The Coach Finder/src/router.js
@@ -4,6 +4,7 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -17,7 +18,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
-    { path: '/:not found(.*)', component: null },
+    { path: '/:not found(.*)', component: NotFound },
   ],
 });
 
