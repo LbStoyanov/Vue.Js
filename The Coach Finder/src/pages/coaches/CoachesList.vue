765dc3cc731d2428cf80dc0cf48aef5f1abe3104
LbStoyanov <lbstoyanov1@gmail.com>
2023-05-31 22:10:35 +0300

    Adding store logic

--- The Coach Finder/src/pages/coaches/CoachesList.vue
+++ The Coach Finder/src/pages/coaches/CoachesList.vue
@@ -1,14 +1,23 @@
 <template>
-    <section>
-        FILTER
-    </section>
-    <section>
-        <div class="controls">
-            <button>Refresh coaches</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul>
-            LIST OF COACHES
-        </ul>
-    </section>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh coaches</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      <li v for="coach in filteredCoaches" :key="coach.id">
+    {{ coach.firstName }}</li>
+    </ul>
+  </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches(){
+        return this.$store.getters['coaches/coaches'];
+    }
+  }
+};
+</script>
